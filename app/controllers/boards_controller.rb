6faadbcb70c8af10c7b65e17d4bffd891f610c37
小川敦司 <>
2023-01-11 21:33:44 +0900

    board一覧ページ haml作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,20 +1,24 @@
 class BoardsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-def new
-    @board = current_user.boards.build
-end
-
-def create
-	@board = Board.new(board_params)
-	if @board.save
-        redirect_to board_path(@board)
+    def index
+        @boards =Board.all
     end
-end
 
-private
-def board_params
-    params.require(:board).permit(:name, :description)
-end
+    def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = Board.new(board_params)
+        if @board.save
+            redirect_to board_path(@board)
+        end
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:name, :description)
+    end
 
 end
