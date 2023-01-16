52a98c60f368754b35a90e8ea8e3daa0afdf3dda
小川敦司 <>
2023-01-16 21:32:44 +0900

    更新機能・画面の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+    before_action :set_board, only: [:show, :edit, :update]
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
@@ -14,6 +15,26 @@ class BoardsController < ApplicationController
         if @board.save
             redirect_to board_path(@board)
         end
+    end
+
+    def edit
+        @board = Board.find(params[:id])
+    end
+
+    def update
+        if @board.update(board_params)
+            redirect_to board_path(@board), notice: '更新できました'
+        else
+            flash.now[:error] = '更新に失敗しました'
+            render :edit
+        end
+    end
+
+    def show
+    end
+
+    def set_board
+        @board = Board.find(params[:id])
     end
 
     private
