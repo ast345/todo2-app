fde3d1dca32b7086759d054eb7da7a22e793f067
小川敦司 <>
2023-01-24 21:53:09 +0900

    コメントの保存ができるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,21 @@ class CommentsController < ApplicationController
         task = Task.find(params[:task_id])
         @comment = task.comments.build
     end
+
+    def create
+        task = Task.find(params[:task_id])
+        @comment = current_user.comments.new(comment_params)
+        @comment.task_id = task.id
+        if @comment.save
+            redirect_to board_task_path(task.board_id, task), notice: 'コメントを追加'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+    private
+    def comment_params
+	    params.require(:comment).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_one :profile, dependent: :destroy
   has_many :tasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :content, presence: true
 
   def avatar_image
